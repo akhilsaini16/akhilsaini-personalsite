51ae9611bb2213607c007de90ee72a100dcc3d99
aks18009 <akhil.saini@uconn.edu>
2024-05-13 19:30:20 -0400

    started navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import About from "@/components/about";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-blue-600 container mx-auto px-12 py-12">
-      <About />
+    <main className="flex min-h-screen flex-col bg-blue-600 mx-auto">
+      <Navbar />
+      <div className="mt-24 px-12 py-12"> <About /></div>
     </main>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+        <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-8">
+            <Link href={"/"} className="text-3xl text-white font-semibold">
+                LOGO
+            </Link>
+            <div className="menu">
+                <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-8">
+                    <li>
+                        <Link href={"#about"} 
+                        className="block py-2 pl-3 pr-4 sm:text-xl rounded md:p-0 hover:text-white">
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  );
+}
