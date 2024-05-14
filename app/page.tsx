a553a67a32075095d7da56a5adb02ca5f77c27f5
aks18009 <akhil.saini@uconn.edu>
2024-05-13 23:03:44 -0400

    added more projects

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-blue-600 mx-auto">
       <Navbar />
       <div className="mt-24 px-12 py-12"> <Intro /></div>
-      {/* <div style={{ marginTop: '700px' }}></div> */}
+      <div style={{ marginTop: '200px' }}></div>
       {/* <About /> */}
       <ProjectsSection />
     </main>
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,115 +1,85 @@
 import React from "react";
 import {Card, CardHeader, CardBody, CardFooter, Image, Button} from "@nextui-org/react";
 
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+    {
+        id: 1,
+        title: "Full Stack Developer",
+        description: "AWS Cool Project",
+        tag: ["Web"],
+    },
+    {
+        id: 2,
+        title: "Front Stack Developer",
+        description: "AWS bad Project",
+        tag: ["Web"],
+    }
+]
+
+
 export default function ProjectsSection() {
-  return (
-    <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
-        <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-4.jpeg"
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-        <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-3.jpeg"
-      />
-    </Card>
-    <Card className="col-span-12 sm:col-span-4 h-[300px]">
-      <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
-        <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-2.jpeg"
-      />
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">New</p>
-        <h4 className="text-black font-medium text-2xl">Acme camera</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Card example background"
-        className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-        src="https://nextui.org/images/card-example-6.jpeg"
-      />
-      <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
-        <div>
-          <p className="text-black text-tiny">Available soon.</p>
-          <p className="text-black text-tiny">Get notified.</p>
+    return(
+        <div className="place-self-center">
+        <h1 className="text-center text-5xl mb-20">My Projects</h1>
+        <Card className="place-self-center bg-slate-400 text-center font-bold text-black mt-4 mb-8 md:mb-12 h-[400px] w-[400px]">
+            <CardHeader>
+                <h1 className="text-2xl mb-10">Drug Affordability Application</h1>
+            </CardHeader>
+            <CardBody>
+                <p>Built a drug affordability web-application using a copay algorithm to allow users to find the cheapest drug alternative. Used AWS to configure the backend.</p>
+            </CardBody>
+            <CardFooter>
+                <a className="bg-opacity-20 bg-black">
+                    Skills Coming
+                </a>
+            </CardFooter>
+        </Card>
+
+        <Card className="place-self-center bg-slate-400 text-center font-bold text-black mt-4 mb-8 md:mb-12 h-[400px] w-[400px]">
+            <CardHeader>
+                <h1 className="text-2xl mb-10">Mobile Health Application</h1>
+            </CardHeader>
+            <CardBody>
+                <p>Built a habit tracking Android application where users create mental and physical health goals to pursue</p>
+            </CardBody>
+            <CardFooter>
+                <a className="bg-opacity-20 bg-black">
+                    Skills Coming
+                </a>
+            </CardFooter>
+        </Card>
+
+        <Card className="place-self-center bg-slate-400 text-center font-bold text-black mt-4 mb-8 md:mb-12 h-[400px] w-[400px]">
+            <CardHeader>
+                <h1 className="text-2xl mb-10">Wordle Application</h1>
+            </CardHeader>
+            <CardBody>
+                <p>Used React.js to create a web application which simulates a replica of the famous New York Times wordle game.</p>
+            </CardBody>
+            <CardFooter>
+                <a className="bg-opacity-20 bg-black">
+                    Skills Coming
+                </a>
+            </CardFooter>
+        </Card>
+
+        <Card className="place-self-center text-center font-bold text-black mt-4 mb-8 md:mb-12 h-[400px] w-[400px]">
+            <CardHeader>
+                <h1 className="text-2xl">Personal Site</h1>
+            </CardHeader>
+            <CardBody>
+                <p>This website</p>
+            </CardBody>
+            <CardFooter>
+                <p>Footer</p>
+            </CardFooter>
+        </Card>
         </div>
-        <Button className="text-tiny" color="primary" radius="full" size="sm">
-          Notify Me
-        </Button>
-      </CardFooter>
-    </Card>
-    <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
-        <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
-      </CardHeader>
-      <Image
-        removeWrapper
-        alt="Relaxing app background"
-        className="z-0 w-full h-full object-cover"
-        src="https://nextui.org/images/card-example-5.jpeg"
-      />
-      <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-        <div className="flex flex-grow gap-2 items-center">
-          <Image
-            alt="Breathing app icon"
-            className="rounded-full w-10 h-11 bg-black"
-            src="https://nextui.org/images/breathing-app-icon.jpeg"
-          />
-          <div className="flex flex-col">
-            <p className="text-tiny text-white/60">Breathing App</p>
-            <p className="text-tiny text-white/60">Get a good night's sleep.</p>
-          </div>
-        </div>
-        <Button radius="full" size="sm">Get App</Button>
-      </CardFooter>
-    </Card>
-  </div>
-  );
+    )
+
 }
-
-// import ProjectCard from "./ProjectCard";
-
-// const projectsData = [
-//     {
-//         id: 1,
-//         title: "Full Stack Developer",
-//         description: "AWS Cool Project",
-//         tag: ["Web"],
-//     },
-//     {
-//         id: 2,
-//         title: "Front Stack Developer",
-//         description: "AWS bad Project",
-//         tag: ["Web"],
-//     }
-// ]
-
-
-// export default function ProjectsSection() {
 //     return (
 //         <>
 //             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
