e7d22a191fbac07d6bc79bc31adb1d62a6ba9184
aks18009 <akhil.saini@uconn.edu>
2024-05-15 22:01:51 -0400

    organized skills

--- components/about.tsx
+++ components/about.tsx
@@ -1,30 +1,59 @@
 import Link from "next/link"
 import SlideUp from "./slide-up"
 import { HiArrowDown } from "react-icons/hi"
-const skills = [
+const Languages = [
     {skill: "Python"},
     {skill: "Java"},
     {skill: "R"},
     {skill: "C"},
     {skill: "C++"},
-    {skill: "Node.js"},
-    {skill: "Docker"},
-    {skill: "AWS Cloud Technologies"},
-    {skill: "RestAPI"},
-    {skill: "Flutter"},
-    {skill: "UI Path"},
-    {skill: "CI/CD"},
-    {skill: "Kubernetes"},
     {skill: "SQL"},
-    {skill: "HTML"},
-    {skill: "CSS"},
-    {skill: "Git"},
-    {skill: "GitHub"},
-    {skill: "React"},
-    {skill: "Next.js"},
     {skill: "TypeScript"},
     {skill: "JavaScript"},
 ]
+const Development = [
+  {skill: "Node.js"},
+  {skill: "RestAPI"},
+  {skill: "Flutter"},
+  {skill: "HTML"},
+  {skill: "CSS"},
+  {skill: "React"},
+  {skill: "Next.js"},
+]
+
+const InfrastructureAndDevOps = [
+  {skill: "Docker"},
+  {skill: "AWS Cloud Technologies"},
+  {skill: "CI/CD"},
+  {skill: "Kubernetes"},
+  {skill: "Git"},
+  {skill: "GitHub"},
+  {skill: "GitHub Actions"},
+]
+// const skills = [
+//   {skill: "Python"},
+//   {skill: "Java"},
+//   {skill: "R"},
+//   {skill: "C"},
+//   {skill: "C++"},
+//   {skill: "Node.js"},
+//   {skill: "Docker"},
+//   {skill: "AWS Cloud Technologies"},
+//   {skill: "RestAPI"},
+//   {skill: "Flutter"},
+//   {skill: "UI Path"},
+//   {skill: "CI/CD"},
+//   {skill: "Kubernetes"},
+//   {skill: "SQL"},
+//   {skill: "HTML"},
+//   {skill: "CSS"},
+//   {skill: "Git"},
+//   {skill: "GitHub"},
+//   {skill: "React"},
+//   {skill: "Next.js"},
+//   {skill: "TypeScript"},
+//   {skill: "JavaScript"},
+// ]
 
 export default function About() {
     return (
@@ -42,9 +71,56 @@ export default function About() {
                 </h1>
               </div>
               <div className="text-center md:w-1/2 md:text-left">
-                <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+                <h1 className="text-2xl font-bold mb-6">Skills</h1>
                 <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                  <div>
+                    <div className="mb-6">
+                      <h1 className="text-xl font-bold">Languages</h1>
+                      <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                        {Languages.map((item, idx) => {
+                          return (
+                            <p
+                              key={idx}
+                              className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                            >
+                              {item.skill}
+                            </p>
+                          )
+                        })}
+                      </div>
+                    </div>
+                    <div className="mb-6">
+                      <h1 className="text-xl font-bold">Development</h1>
+                      <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                        {Development.map((item, idx) => {
+                          return (
+                            <p
+                              key={idx}
+                              className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                            >
+                              {item.skill}
+                            </p>
+                          )
+                        })}
+                      </div>
+                    </div>
+                    <div className="mb-6">
+                      <h1 className="text-xl font-bold">Infrastructure and DevOps</h1>
+                      <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                        {InfrastructureAndDevOps.map((item, idx) => {
+                          return (
+                            <p
+                              key={idx}
+                              className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                            >
+                              {item.skill}
+                            </p>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                {/* <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
                   {skills.map((item, idx) => {
                     return (
                       <p
@@ -55,7 +131,7 @@ export default function About() {
                       </p>
                     )
                   })}
-                </div>
+                </div> */}
                 </SlideUp>
               </div>
             </div>
