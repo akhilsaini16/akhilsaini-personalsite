4fb1545b2ddb3733588c74ed6c00fdfc5a2664e5
aks18009 <akhil.saini@uconn.edu>
2025-02-24 17:35:01 -0500

    edited navbar for smaller screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,14 @@ export default function Navbar() {
   const items = ['About', 'Projects']
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-90" id="Navbar">
-        <div className="flex flex-wrap items-center justify-between mx-auto py-4 px-8">
+        <div className="flex flex-wrap items-center justify-between mx-auto py-2 sm:py-4 px-4 sm:px-8">
             <a href={"/"} className="text-3xl text-white font-semibold">
                 Akhil Saini
             </a>
-            <div className="flex p-4 md:p-0 sm:flex-row md:space-x-8">
+            <div className="flex sm:p-0 sm:flex-row md:space-x-8">
                 {items.map((item) =>(
                     <a
-                        className="block py-2 pl-3 pr-4 sm:text-xl rounded md:p-0 hover:text-white"
+                        className="block py-2 pl-3 sm:text-xl rounded md:p-0 hover:text-white"
                         href={`#${item}`} key={item}
                     >
                         {item}
